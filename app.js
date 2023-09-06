781a06469565fa403e76d85ceaff1ac4aa1d4c48
up885000 <up885000@myport.ac.uk>
2023-09-06 20:29:16 +0100

    test2

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // let com = [];
 let balloon_data = [];
 jQuery.get('Balloon_Data.csv', function(data) {
+    console.log(data);
     com = data.split("\r\n");
     console.log(com);
     for (i in com) {
+        console.log(com[i]);
         balloon = com[i].split(",");
         balloon_data.push(balloon);
     }
-    balloon_data.shift();
-    balloon_data.pop();
+    // balloon_data.shift();
+    // balloon_data.pop();
     console.log(balloon_data);
     document.getElementById("test").textContent=balloon_data;
 });

6f7a1ac8998f17a17c97cac290477ec22ed5c6d2
b4bjw123 <b4bjw123@gmail.com>
2023-09-06 18:51:10 +0100

    Added Ballon Data

---
+++ app.js
@@ -0,0 +1,14 @@
+// let com = [];
+let balloon_data = [];
+jQuery.get('Balloon_Data.csv', function(data) {
+    com = data.split("\r\n");
+    // console.log(com);
+    for (i in com) {
+        balloon = com[i].split(",");
+        balloon_data.push(balloon);
+    }
+    balloon_data.shift();
+    balloon_data.pop();
+    // console.log(balloon_data);
+    document.getElementById("test").textContent=balloon_data;
+});
